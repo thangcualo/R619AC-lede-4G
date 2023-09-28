3c1e9a77fd0433be6d45677e2031d13287d53202
thangcualo <122159126+thangcualo@users.noreply.github.com>
2023-09-29 06:49:03 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,9 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-git clone https://github.com/kenzok8/small.git package/small
-git clone https://github.com/open-mesh-mirror/batman-adv.git package/batman-adv
-# Add theme
-rm -rf feeds/luci/applications/luci-app-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-rm -rf feeds/luci/applications/luci-app-argon-config && git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
+src-git small https://github.com/Aibx/OpenWRT-Packages
